d4cc2daefe89f2e859f1ffad7118b7e9401cd1a3
Patrick <patrick.desenv@gmail.com>
2022-03-03 21:56:26 -0300

    Adding new project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Calculadora de IMC
+
+Projeto feito em ReactJS + TypeScript, fazendo uma Calculadora de IMC.
+
+## Instalação 
+ - `npm install`
+
+## Para rodar 
+- `npm start`  
---
+++ src/helpers/imc.ts
@@ -0,0 +1,48 @@
+export type Level = {
+    title: string;
+    color: string;
+    icon: 'down' | 'up';
+    imc: number[];
+    yourImc?: number;
+
+}
+
+export const levels: Level[] = [
+    {
+        title: 'Magreza',
+        color: '#96a3ab',
+        icon: 'down',
+        imc: [0, 18.5]
+    },
+    {
+        title: 'Normal',
+        color: '#0ead69',
+        icon: 'up',
+        imc: [18.5, 24.9]  
+    },
+    {
+        title: 'Sobrepeso',
+        color: '#e2b036',
+        icon: 'down',
+        imc: [25, 30]  
+    },
+    {
+        title: 'Obesidade',
+        color: '#c3423f',
+        icon: 'down',
+        imc: [30.1, 99]  
+    }
+];
+
+export const calculateImc = (height: number, weigth: number) => {
+    const imc = weigth / Math.pow(height,2);
+
+    for(let i in levels) {
+        if( imc >= levels[i].imc[0] && imc < levels[i].imc[1]) {
+            let levelCopy: Level = {...levels[i]};
+            levelCopy.yourImc = parseFloat(imc.toFixed(2));
+            return levelCopy;
+        }
+    } 
+    return null;
+}
